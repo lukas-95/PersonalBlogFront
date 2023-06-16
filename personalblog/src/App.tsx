34b60adb848964ad232a59d78f6af70a5c08528e
lukas-95 <lucrecmen@gmail.com>
2023-06-16 20:32:07 -0300

    add routes

--- personalblog/src/App.tsx
+++ personalblog/src/App.tsx
@@ -5,13 +5,23 @@ import { Grid } from "@material-ui/core";
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Navbar from './components/statics/navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: "100vh" }}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+
       <Footer />
-    </>
+    </BrowserRouter>
   );
 
 }
