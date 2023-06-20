1c7fb78f18bd3871030b9b8ea7bd4b2f109bdd1e
lukas-95 <lucrecmen@gmail.com>
2023-06-20 20:04:03 -0300

    add login page

--- personalblog/src/pages/login/Login.tsx
+++ personalblog/src/pages/login/Login.tsx
@@ -1,76 +1,120 @@
-import React from "react";
-import './Login.css';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import "./Login.css";
 import { Grid, Box, Typography, TextField, Button } from "@material-ui/core";
-import { Link } from "react-router-dom";
-import Skills from '../../assets/Banner-linkedIn-com-gradiente-prisma.svg'; // Import using relative path
+import { Link, useNavigate } from "react-router-dom";
+import Skills from "../../assets/Banner-linkedIn-com-gradiente-prisma.svg"; // Import using relative path
+import useLocalStorage from "react-use-localstorage";
+import UserLogin from "../../models/UserLogin";
+import { login } from "../../services/service";
+import CadastroUsuario from "../cadastroUsuario/CadastroUsuario.tsx";
 
 export default function Login() {
+  let navigate = useNavigate();
 
+  const [token, setToken] = useLocalStorage('token');
+  const [userLogin, setUserLogin] = useState<UserLogin>({
+    id: 0,
+    usuario: "",
+    senha: "",
+    token: ""
+  });
 
-    return (
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid alignItems="center" xs={6}>
-          <Box paddingX={20}>
-            <form action="">
-              <Typography
-                variant="h3"
-                gutterBottom
-                color="primary"
-                align="center"
-                style={{ fontWeight: "bold" }}
-              >
-                Entrar
-              </Typography>
-              <TextField
-                id="user"
-                label="user"
-                variant="outlined"
-                name="usuario"
-                margin="normal"
-                fullWidth
-              />
-              <TextField
-                id="password"
-                label="password"
-                variant="outlined"
-                name="password"
-                margin="normal"
-                type="password"
-                fullWidth
-              />
-              <Box marginTop={2} textAlign='center'>
+  function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+    setUserLogin({
+      ...userLogin,
+      [e.target.name]: e.target.value,
+    });
+    console.log(JSON.stringify(userLogin));
+  }
 
-                <Link to='/home' className='text-decorator-none'>
-                <Button type='submit' variant='contained' color='primary'>
-                    Logar
+  useEffect(() => {
+    if (token != "") {
+      navigate("/home");
+    }
+  }, [token]);
+
+  async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      await login(`usuarios/logar`, userLogin, setToken);
+      alert("Bem-vindo, você ja está logado com sucesso");
+    } catch (error) {
+      alert(
+        "Oops, o e-mail ou a senha estão erradas, confira e tente novamente"
+      );
+    }
+  }
+
+  // @ts-ignore
+  return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid alignItems="center" xs={6}>
+        <Box paddingX={20}>
+          <form onSubmit={onSubmit}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="primary"
+              align="center"
+              style={{ fontWeight: "bold" }}
+            >
+              Entrar
+            </Typography>
+            <TextField
+              value={userLogin.usuario}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+              id="user"
+              label="user"
+              variant="outlined"
+              name="usuario"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              value={userLogin.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+              id="password"
+              label="password"
+              variant="outlined"
+              name="senha"
+              margin="normal"
+              type="password"
+              fullWidth
+            />
+            <Box marginTop={2} textAlign="center">
+             
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  className="text" >
+                  Logar
                 </Button>
-                </Link>
-              </Box>
-            </form>
-            <Box display='flex' justifyContent='center' marginTop={2}>
-                <Box marginRight={1}>
-                    <Typography variant="subtitle1" gutterBottom align="center"> Não tem uma conta ?
-
-                    </Typography>
-                </Box>
-                <Typography variant="subtitle1" gutterBottom align="center" style={{fontWeight: 'bold'}}> Cadastre-se agora mesmo
-
-                </Typography>
+              
             </Box>
+          </form>
+          <Box display="flex" justifyContent="center" marginTop={2}>
+            <Box marginRight={1}>
+              <Typography variant="subtitle1" gutterBottom align="center">
+                {" "}
+                Não tem uma conta ?
+              </Typography>
+            </Box>
+            <Link to="/cadastrousuario">
+              <Typography
+                variant="subtitle1"
+                gutterBottom
+                align="center"
+                className="text"
+              >
+                {" "}
+                Cadastre-se agora mesmo
+              </Typography>
+            </Link>
           </Box>
-        </Grid>
-        <Grid xs={6} style={{
-            backgroundImage: 'url (${Skills})',
-            backgroundRepeat: 'no-repeat', width: '100vh', backgroundSize: 'cover', backgroundPosition: 'center'
-        }}>
-
-
-        </Grid>
+        </Box>
       </Grid>
-    );
+      <Grid item xs={6} className="image"></Grid>
+    </Grid>
+  );
 }
