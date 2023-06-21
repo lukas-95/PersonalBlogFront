3fb1ba5e6411285409f6d7b3eb9b2734afa76d1a
lukas-95 <lucrecmen@gmail.com>
2023-06-21 15:34:02 -0300

    add rules for cadastroUsuario

--- personalblog/src/services/service.ts
+++ personalblog/src/services/service.ts
@@ -4,11 +4,14 @@ export const api = axios.create({
     baseURL: 'https://personalblog-ly4z.onrender.com'
 })
 
+export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
+  const resposta = await api.post(url, dados);
+  setDado(resposta.data);
+}
 
 export const login = async(url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
-    setDado(resposta.data.token
-        )
+    setDado(resposta.data.token)
 
 
 }
