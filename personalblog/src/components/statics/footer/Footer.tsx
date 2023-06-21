7ddd39f3c32aa117f6be305c5f700140cdc82519
lukas-95 <lucrecmen@gmail.com>
2023-06-21 15:29:51 -0300

    style on Footer

--- personalblog/src/components/statics/footer/Footer.tsx
+++ personalblog/src/components/statics/footer/Footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import GitHubIcon from '@material-ui/icons/GitHub';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
@@ -8,33 +7,30 @@ import {Box} from '@mui/material';
 function Footer() {
     return (
       <>
-        <Grid container direction="row" justifyContent="center" alignItems="center">
+
+          <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box className="box1">
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className="text" >Follow me</Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.linkedin.com/in/lucas-reck-dev/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 30, color: "white" }} />
+                                <LinkedInIcon className="social" />
                             </a>
                             <a href="https://github.com/lukas-95" target="_blank">
-                                <GitHubIcon style={{ fontSize: 25, color: "white" }} />
+                                <GitHubIcon className="social" />
                             </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 30, color: "white" }} />
+                            <a href="https://www.instagram.com/recklucas/" target="_blank">
+                                <InstagramIcon className="social" />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className="box2">
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className="text" >© 2023 by Lucas</Typography>
                         </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
-                            </a>
-                        </Box>
+
                     </Box>
                 </Grid>
             </Grid>
