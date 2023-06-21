dd9e7b425b63ee968113a5db214f42d2b159f2c7
lukas-95 <lucrecmen@gmail.com>
2023-06-21 15:32:21 -0300

    remove home import

--- personalblog/src/main.tsx
+++ personalblog/src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.css'
-import Home from './pages/home/Home.tsx'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-<App />
-</React.StrictMode>,
-document.getElementById('root') as HTMLElement
+    <React.StrictMode>
 
+            <App/>
+    </React.StrictMode>,
+    document.getElementById('root') as HTMLElement
 )
