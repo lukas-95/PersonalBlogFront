7d7ba5549dc15cbaee41209acb35caf18c20c8a1
lukas-95 <lucrecmen@gmail.com>
2023-06-21 15:31:19 -0300

    style on Navbar

--- personalblog/src/components/statics/navbar/Navbar.tsx
+++ personalblog/src/components/statics/navbar/Navbar.tsx
@@ -113,7 +113,7 @@ export default function Navbar() {
           >
             <MenuIcon />
           </IconButton>
-          <Box style={{ cursor: "pointer" }}>
+          <Box className='cursor'>
             <Typography variant="h5" color="inherit">
               BlogPessoal
             </Typography>
@@ -121,30 +121,30 @@ export default function Navbar() {
 
           <Box display="flex" justifyContent="start">
             <Link to="/home" className="text-decorator-none">
-              <Box mx={1} style={{ cursor: "pointer" }}>
+              <Box mx={1} className='cursor'>
                 <Typography variant="h6" color="inherit">
                   home
                 </Typography>
               </Box>
             </Link>
 
-            <Box mx={1} style={{ cursor: "pointer" }}>
+            <Box mx={1} className='cursor'>
               <Typography variant="h6" color="inherit">
                 postagens
               </Typography>
             </Box>
-            <Box mx={1} style={{ cursor: "pointer" }}>
+            <Box mx={1} className='cursor'>
               <Typography variant="h6" color="inherit">
                 temas
               </Typography>
             </Box>
-            <Box mx={1} style={{ cursor: "pointer" }}>
+            <Box mx={1} className='cursor'>
               <Typography variant="h6" color="inherit">
                 cadastrar tema
               </Typography>
             </Box>
             <Link to="/login" className="text-decorator-none">
-              <Box mx={1} style={{ cursor: "pointer", color: "white" }}>
+              <Box mx={1} className='cursor'>
                 <Typography variant="h6" color="inherit">
                   logout
                 </Typography>
