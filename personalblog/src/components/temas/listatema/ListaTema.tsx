8dd1e51738129c6ac68386667aadf3f8bf0d2d44
lukas-95 <lucrecmen@gmail.com>
2023-06-26 14:46:29 -0300

    add component temas

--- personalblog/src/components/temas/listatema/ListaTema.tsx
+++ personalblog/src/components/temas/listatema/ListaTema.tsx
@@ -1,45 +1,83 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import {Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import {Box} from '@mui/material';
 import './ListaTema.css';
+import useLocalStorage from 'react-use-localstorage';
+import { busca } from '../../../services/service';
+import Tema from '../../../models/Tema';
 
 function ListaTema() {
+    const [temas, setTemas] = useState<Tema[]>([])
+    const [token, setToken] = useLocalStorage('token');
+    let navigate = useNavigate();
+  
+    useEffect(()=>{
+      if(token == ''){
+        alert("Faça login e tente novamente")
+        navigate("/login")
+      }
+    }, [token])
+  
+  
+    async function getTema(){
+        await busca("/temas", setTemas, {
+        headers: {
+          'Authorization': token
+        }
+      })
+    }
+  
+  
+    useEffect(()=>{
+      getTema()
+    }, [temas.length])
+
+
 
     return (
         <>
-            <Box m={2} >
-                <Card variant="outlined">
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>
-                            Tema
-                        </Typography>
-                        <Typography variant="h5" component="h2">
-                            Minha descrição
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Box display="flex" justifyContent="center" mb={1.5} >
 
-                            <Link to="" className="text-decorator-none">
-                                <Box mx={1}>
-                                    <Button variant="contained" className="marginLeft" size='small' color="primary" >
-                                        atualizar
-                                    </Button>
-                                </Box>
-                            </Link>
-                            <Link to="" className="text-decorator-none">
-                                <Box mx={1}>
-                                    <Button variant="contained" size='small' color="secondary">
-                                        deletar
-                                    </Button>
-                                </Box>
-                            </Link>
+        {
+            temas.map (tema => (
+        
+                <Box m={2} >
+                <Card variant="outlined">
+                  <CardContent>
+                    <Typography color="textSecondary" gutterBottom>
+                      Tema
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                     {tema.descricao}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Box display="flex" justifyContent="center" mb={1.5} >
+        
+                      <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+                        <Box mx={1}>
+                          <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                            atualizar
+                          </Button>
                         </Box>
-                    </CardActions>
+                      </Link>
+                      <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+                        <Box mx={1}>
+                          <Button variant="contained" size='small' color="secondary">
+                            deletar
+                          </Button>
+                        </Box>
+                      </Link>
+                    </Box>
+                  </CardActions>
                 </Card>
-            </Box>
+              </Box>
+
+            ))
+
+}
         </>
+
     );
 }
 
