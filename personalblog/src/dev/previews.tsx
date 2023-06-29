dcf3ee9b30f4c75ee39a4c50b7c1773521447cf0
lukas-95 <lucrecmen@gmail.com>
2023-06-29 08:47:26 -0300

    add dev previews

--- personalblog/src/dev/previews.tsx
+++ personalblog/src/dev/previews.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import {Previews} from "@react-buddy/ide-toolbox";
+import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
 import {PaletteTree} from "./palette";
+import Home from "../pages/home/Home.tsx";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/Home">
+                <Home/>
+            </ComponentPreview>
         </Previews>
     );
 };
